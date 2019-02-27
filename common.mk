a59664eab2805b3c6bbcdbdf11d44be250aa027f
Thibaut Le Page <thibaut.le.page@zalando.de>
2019-02-27 16:11:56 +0100

    common.mk: split "test" into "unittest" + "functest"
    Signed-off-by: Thibaut Le Page <thibaut.le.page@zalando.de>

--- common.mk
+++ common.mk
@@ -1,19 +1,27 @@
 # This file is included in both Makefile.local & Makefile.ci.
 
 SRC := $(shell find transformer/ -name '*.py' ! -name 'test_*' ! -name 'builders_*' )
+DIST := pyproject.toml poetry.lock
 
 # Runs "poetry install" if pyproject.toml or poetry.lock have changed.
 .PHONY: configure
 configure: .make/configure
 
-.make/configure: pyproject.toml poetry.lock
+.make/configure: $(DIST)
 	poetry install
 	mkdir -p .make && touch .make/configure
 
 # Runs pytest with coverage reporting.
-.PHONY: test
-test: configure
+.PHONY: unittest
+unittest: configure
 	poetry run pytest --failed-first --cov-config .coveragerc --cov-report xml --cov=. transformer/
+
+.PHONY: functest
+functest: configure
+	poetry run python functional-tests/run.py
+
+.PHONY: test
+test: unittest functest
 
 .PHONY: lint
 lint: black flake8
