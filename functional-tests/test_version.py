9ceb79b9586b5f07ac0deb0339bb493c848eec23
Thibaut Le Page <thibaut.le.page@zalando.de>
2019-03-06 17:24:18 +0100

    functional-tests/test_version: update expected version
    Signed-off-by: Thibaut Le Page <thibaut.le.page@zalando.de>

--- functional-tests/test_version.py
+++ functional-tests/test_version.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 def test_version():
-    expected = "1.0.2"
+    expected = "1.1.0"
     actual = (
         subprocess.run(["transformer", "--version"], check=True, stdout=subprocess.PIPE)
         .stdout.strip()
