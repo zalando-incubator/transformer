556b724bee924fa83cc745f41a3b74c3693cf46e
Thibaut Le Page <thilp@thilp.net>
2019-11-14 10:56:04 +0100

    tests/functional: add test showing log destination
    Signed-off-by: Thibaut Le Page <thilp@thilp.net>

--- tests/functional/test_examples.py
+++ tests/functional/test_examples.py
@@ -2,11 +2,15 @@ import subprocess
 from pathlib import Path
 
 
-def run_example(example_name: str) -> subprocess.CompletedProcess:
+def run_example(example_name: str, **subprocess_opts) -> subprocess.CompletedProcess:
     har_path = Path(__file__).parent.parent.parent.joinpath("examples", example_name)
-    return subprocess.run(
-        ["transformer", str(har_path)], timeout=2, check=True, stdout=subprocess.PIPE
-    )
+    subprocess_opts = {
+        "timeout": 2,
+        "check": True,
+        "stdout": subprocess.PIPE,
+        **subprocess_opts,
+    }
+    return subprocess.run(["transformer", str(har_path)], **subprocess_opts)
 
 
 def test_example_google():
@@ -19,3 +23,12 @@ def test_example_zalando():
     ex = run_example("en.zalando.de.har")
     assert len(ex.stdout) > 100
     assert b"https://en.zalando.de/?_rfl=de" in ex.stdout
+
+
+def test_logs_in_stderr():
+    ex = run_example(
+        "en.zalando.de.har", stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    assert len(ex.stdout) > 100
+    assert b"INFO" not in ex.stdout
+    assert b"INFO" in ex.stderr
