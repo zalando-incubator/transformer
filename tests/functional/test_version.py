5d7393559c105232d37e5fd4ce002eb37b5a5baf
Thibaut Le Page <thilp@thilp.net>
2020-06-05 10:41:13 +0200

    test_version: support alpha/beta markers

--- tests/functional/test_version.py
+++ tests/functional/test_version.py
@@ -3,7 +3,9 @@ import subprocess
 
 
 def test_version():
-    expected_pattern = re.compile(r"\b [0-9]+ \. [0-9]+ \. [0-9]+ \b", re.X)
+    expected_pattern = re.compile(
+        r"\b [0-9]+ \. [0-9]+ \. [0-9]+ ([ab] [0-9]+)? \b", re.X
+    )
     actual = (
         subprocess.run(["transformer", "--version"], check=True, stdout=subprocess.PIPE)
         .stdout.strip()
